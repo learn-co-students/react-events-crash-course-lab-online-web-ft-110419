8d5ba3f4dc8203007c5a446efbba1fb42f15629b
Richell Bonfield <richell.bonfield12@gmail.com>
2020-04-02 21:02:35 +0000

    Done.

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -13,7 +13,7 @@ export default class ChromeBoisDomain extends Component {
      
      drawChromeBoiAtCoords(event.clientX, event.clientY)
   }
-  
+  #
   handleKeyDown = (event) => {
     if (event.key === 'a') {
       resize('+')
